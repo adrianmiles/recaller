bddeac78f5cf50689316302557288a160e9c40af
eichos <adrianrossmiles@gmail.com>
2021-03-20 10:09:19 +0200

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import Communications from "react-native-communications";
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  SafeAreaView,
+  Linking,
+} from "react-native";
 
 export default function App() {
+  const [numberToCall, setNumberToCall] = useState("0836024988");
+  const triggerCall = () => {
+    Communications.phonecall(numberToCall, false);
+    // if (numberToCall.length != 10) {
+    //   alert("Please insert correct contact number");
+    //   return;
+    // }
+
+    // Linking.openURL(`tel://${numberToCall}`);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.sectionTitle}>Enter a phone number</Text>
+        <View>
+          <TextInput
+            value={numberToCall}
+            onChangeText={(inputValue) => setNumberToCall(inputValue)}
+            placeholder={"Number to call"}
+            keyboardType="numeric"
+            style={styles.textInput}
+          />
+          <TouchableOpacity
+            activeOpacity={0.7}
+            style={styles.buttonStyle}
+            onPress={triggerCall}
+          >
+            <Text style={styles.buttonTextStyle}>Make a Call</Text>
+          </TouchableOpacity>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  sectionTitle: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  buttonTextStyle: {
+    color: "#fff",
+    textAlign: "center",
+  },
+  buttonStyle: {
+    justifyContent: "center",
+    marginTop: 15,
+    padding: 10,
+    backgroundColor: "#8ad24e",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 22,
+    alignItems: "center",
+    backgroundColor: "white",
+    padding: 10,
+    textAlign: "center",
+  },
+
+  textInput: {
+    width: 200,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 10,
+    height: 40,
+    paddingHorizontal: 10,
   },
 });
